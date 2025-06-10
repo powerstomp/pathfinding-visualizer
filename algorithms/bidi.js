5323f32881ae4cdfb19f18caef8933e1e71d614f
HatsuneKareshi <m1e2a3n4n5@gmail.com>
2025-06-10 14:44:01 +0700

    minor fix to bidirectional BFS to allow faster detection of adjacent start-goal

--- algorithms/bidi.js
+++ algorithms/bidi.js
@@ -11,7 +11,7 @@ function compare_tile(a, b) {
     return (a[TILE_ROW] === b[TILE_ROW] && a[TILE_COL] === b[TILE_COL]);
 }
 
-function __subcompact_bidi_BFS({ map, frtr, vstd, other_vstd, markFrontier, markVisited, startIteration }) {
+function __subcompact_bidi_BFS({ map, frtr, other_frtr, vstd, other_vstd, markFrontier, markVisited, startIteration }) {
     if (frtr.isEmpty())
         return null;
     let curtile = frtr.pop(); // pop from the frontier
@@ -32,11 +32,14 @@ function __subcompact_bidi_BFS({ map, frtr, vstd, other_vstd, markFrontier, mark
             ptt_visit[TILE_COL] < 0 || ptt_visit[TILE_COL] >= map[0].length)
             continue; // this out of bounds
 
-        if (vstd[ptt_visit[0]][ptt_visit[1]] || map[ptt_visit[0]][ptt_visit[1]] || frtr.exists(ptt_visit, compare_tile))
+        if (vstd[ptt_visit[0]][ptt_visit[1]] || (map[ptt_visit[0]][ptt_visit[1]] === true) || frtr.exists(ptt_visit, compare_tile))
             continue; // visited, impassible or already in the frontier
 
         if (other_vstd[ptt_visit[0]][ptt_visit[1]])
             return true; // visited by the other searching side.
+
+        if (map[ptt_visit[TILE_ROW]][ptt_visit[TILE_COL]] === "S" || map[ptt_visit[TILE_ROW]][ptt_visit[TILE_COL]] === "G")
+            return true; // if stumpled upon either start or finish
 
         frtr.push(ptt_visit);
         markFrontier(ptt_visit);
@@ -57,6 +60,7 @@ function bidi_BFS({ map, start, end, markFrontier, markVisited, startIteration }
         let fromStart = __subcompact_bidi_BFS({
             map: map,
             frtr: frontierStart,
+            other_frtr: frontierGoal,
             vstd: visitedStart,
             other_vstd: visitedGoal,
             markFrontier,
@@ -66,6 +70,7 @@ function bidi_BFS({ map, start, end, markFrontier, markVisited, startIteration }
         let fromGoal = __subcompact_bidi_BFS({
             map: map,
             frtr: frontierGoal,
+            other_frtr: frontierStart,
             vstd: visitedGoal,
             other_vstd: visitedStart,
             markFrontier,
