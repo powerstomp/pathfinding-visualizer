957d39562020be7bf97a2b4622c429a10a12e8a7
DreamingShynnevilla <pmtriet23@clc.fitus.edu.vn>
2025-06-13 23:51:24 +0700

    PV: changed BFS

--- algorithms/bfs.js
+++ algorithms/bfs.js
@@ -1,25 +1,31 @@
-function bfs({ map, start, markFrontier, markVisited, startIteration }) {
+function bfs({ map, start, goal, markFrontier, markVisited, startIteration }) {
     let visited = Array.from(Array(map.length), () => new Array(map[0].length));
     let q = new queue();
-    q.push(start);
-    while (!q.isEmpty()) {
-        let u = q.pop();
-        if (visited[u[0]][u[1]])
-            continue;
-        startIteration();
-        visited[u[0]][u[1]] = true;
-        markVisited(u);
+    let parent = Array.from(Array(map.length), () => new Array(map[0].length).fill(null));
 
-        for (let i = 0; i < 4; i++) {
-            let v = [u[0] + [0, 1, 0, -1][i], u[1] + [1, 0, -1, 0][i]];
-            if (v[0] < 0 || v[0] >= map.length || v[1] < 0 || v[1] >= map[0].length)
-                continue;
-            if (map[v[0]][v[1]] === true || visited[v[0]][v[1]])
-                continue;
-            if (map[v[0]][v[1]] === 'G')
-                return;
-            q.push(v);
-            markFrontier(v);
+    q.push(start);
+    visited[start[0]][start[1]] = true;
+
+    while (!q.isEmpty()) {
+        let [x, y] = q.pop();
+        startIteration();
+        markVisited([x, y]);
+        if (x === goal[0] && y === goal[1]) {
+            let trace = [[x, y]];
+            while (parent[x][y]) {
+                [x, y] = parent[x][y];
+                trace.push([x, y]);
+            }
+            return trace.reverse();
+        }
+        for (const [nx, ny] of getAdjacent(map, [x, y])) {
+            if (!visited[nx][ny]) {
+                q.push([nx, ny]);
+                visited[nx][ny] = true;
+                parent[nx][ny] = [x, y];
+                markFrontier([nx, ny]);
+            }
         }
     }
+    return null;
 }
