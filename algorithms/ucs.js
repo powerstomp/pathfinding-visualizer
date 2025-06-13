adb0ecdabb96f1b8de249196dc1c2dcc37fbead1
HatsuneKareshi <m1e2a3n4n5@gmail.com>
2025-06-13 17:55:45 +0700

    re-did ucs to make it compatible

--- algorithms/ucs.js
+++ algorithms/ucs.js
@@ -1,71 +1,106 @@
-let r_mod = [-1, 0, 1, 0];
-let c_mod = [0, 1, 0, -1];
+function new_ucs({ map, start, goal, markFrontier, markVisited, startIteration }) {
+    let explored = Array.from(Array(map.length), () => new Array(map[0].length));
+    let frontier = [[start[0], start[1], 0]]; // this will be replaced with something faster later.
+    let path = [];
+    let path_track = __path_map_gen(map);
+    let found = false;
 
-const TILE_COST = 2;
-const TILE_ROW = 0;
-const TILE_COL = 1;
+    while (frontier.length) {
+        frontier.sort((a, b) => (a[2] - b[2]));
+        let cur_tile = frontier.shift(); // grab tile with smallest weight for evaluation
 
-function __cost_map_generator(map) {
-    let a = Array.from({ length: map.length }, (_, i) =>
-        Array.from({ length: map[0].length }, (_, j) => {
-            let tile = new Array(3);
-            tile[TILE_ROW] = i;
-            tile[TILE_COL] = j;
-            tile[TILE_COST] = 0;
-            return tile;
-        })
-    );
-    return a;
+        if (explored[cur_tile[0]][cur_tile[1]])
+            // skip iteration if tile is already visited
+            continue;
+        if (cur_tile[0] === goal[0] && cur_tile[1] === goal[1]) {
+            found = true;
+            break; // if goal then eject
+        }
+
+        startIteration();
+        explored[cur_tile[0]][cur_tile[1]] = true; // mark tile as visited
+        markVisited(cur_tile);
+
+        for (let potentials of getAdjacent(map, cur_tile)) {
+            potentials.push(map[potentials[0]][potentials[1]] + cur_tile[2]);
+            if (explored[potentials[0]][potentials[1]])
+                continue;
+            if (__frontier_resident_chk(frontier, potentials))
+                continue;
+            frontier.push(potentials);
+            path_track[potentials[0]][potentials[1]] = cur_tile;
+            markFrontier(potentials);
+        }
+    }
+
+    if (found) {
+        let cur = goal;
+        let prev = path_track[cur[0]][cur[1]];
+        while (prev !== null) {
+            path.push([cur[0], cur[1]]);
+            cur = path_track[cur[0]][cur[1]];
+            prev = path_track[cur[0]][cur[1]];
+        }
+        path.push(start);
+    }
+
+    return (path.length === 0) ? null : path; // for now this returns null
 }
 
-function __frontier_resident_chk_n_update(frtr, tile) {
+function __path_map_gen(map) {
+    return Array.from({ length: map.length }, () =>
+        Array.from({ length: map[0].length }, () => null)
+    );
+}
+
+function __frontier_resident_chk(frtr, tile) {
     for (let tl of frtr) {
-        if (tl[TILE_COL] === tile[TILE_COL] && tl[TILE_ROW] === tile[TILE_ROW]) {
-            if (tile[TILE_COST] < tl[TILE_COST]) {
-                tl[TILE_COST] = tile[TILE_COST];
-            }
+        if (tl[0] === tile[0] && tl[1] === tile[1]) {
+            if (tl[2] > tile[2])
+                tl[2] = tile[2];
             return true;
         }
     }
     return false;
 }
 
-function ucs({ map, start, end, markFrontier, markVisited, startIteration }) { // for now this is a breadthfs, since theres no cost consideration yet.
+// const recursive_guts = (cur) => {
 
-    let _start_wcost = Array.from(start);
-    _start_wcost.push(0);
-    let frontier = [_start_wcost];
-    let explored = Array.from(Array(map.length), () => new Array(map[0].length));
-    while (1) {
-        frontier.sort((a, b) => (a[TILE_COST] - b[TILE_COST])); // this will for now sort alphabetically. not goop. it has to sort by cost
 
-        if (frontier.length === 0) // that should be a failure
-            return;
+//     if (explored[cur[0]][cur[1]]) {
+//         return false;
+//     }
 
-        let cur_tile = frontier.shift();
+//     path.push(cur);
 
-        if (explored[cur_tile[0]][cur_tile[1]]) // this how we check explored
-            continue;
+//     if (cur[0] === goal[0] && cur[1] === goal[1])
+//         return true;
 
-        startIteration(); // this likely is a function to mark where the current iteration is going
-        explored[cur_tile[0]][cur_tile[1]] = true;
-        markVisited(cur_tile);
+//     startIteration();
+//     explored[cur[0]][cur[1]] = true;
+//     markVisited(cur);
 
-        for (let i = 0; i < 4; i++) {
-            let potential_visit = [cur_tile[0] + r_mod[i], cur_tile[1] + c_mod[i], cur_tile[TILE_COST] + 1];
+//     for (neighbors of getAdjacent(map, cur)) {
+//         neighbors.push(map[neighbors[0]][neighbors[1]] + cur[2]);
+//         if (explored[neighbors[0]][neighbors[1]]) // if explored
+//             continue;
+//         if (__frontier_resident_chk(frontier, neighbors)) // if in frontier
+//             continue;
+//         frontier.push(neighbors);
+//         markFrontier(neighbors);
+//     }
 
-            if (potential_visit[0] >= map.length || potential_visit[0] < 0
-                || potential_visit[1] >= map[0].length || potential_visit[1] < 0)
-                continue; // out of bounds
+//     if (frontier.length === 0)
+//         return false;
+//     else {
+//         frontier.sort((a, b) => (a[2] - b[2]));
+//         next = frontier.shift();
+//         if (recursive_guts(next))
+//             return true;
+//     }
 
-            if (explored[potential_visit[0]][potential_visit[1]] || map[potential_visit[0]][potential_visit[1]] === true || __frontier_resident_chk_n_update(frontier, potential_visit))
-                continue; // already did, or its in the frontier
+//     path.pop();
+//     return false;
+// }
 
-            if (map[potential_visit[0]][potential_visit[1]] === 'G')
-                return;
-
-            frontier.push(potential_visit);
-            markFrontier(potential_visit);
-        }
-    }
-}
+// recursive_guts([start[0], start[1], 0]);
