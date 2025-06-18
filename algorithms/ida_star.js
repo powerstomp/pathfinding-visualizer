1ac87e4243432b66639f1ccac69d653455217e38
powerstomp <119428043+powerstomp@users.noreply.github.com>
2025-06-18 22:10:43 +0700

    IDA* rendering changes and speedup

--- algorithms/ida_star.js
+++ algorithms/ida_star.js
@@ -1,9 +1,13 @@
-function search({ map, g, threshold, path, node, goal, markFrontier, markVisited }) {
+function search({ map, g, threshold, path, node, goal, markFrontier, markVisited, visited }) {
     let f = g + heuristic(node, goal);
-    markFrontier(node);
-    if (f > threshold)
+    if (f > threshold) {
+        if (!visited[node[0]][node[1]])
+            markFrontier(node);
         return f;
-
+    } else if (!visited[node[0]][node[1]]) {
+        markVisited(node);
+        visited[node[0]][node[1]] = true;
+    }
 
     if (node[0] === goal[0] && node[1] === goal[1])
         return true;
@@ -13,24 +17,23 @@ function search({ map, g, threshold, path, node, goal, markFrontier, markVisited
         if (path.some(p => p[0] === neighbor[0] && p[1] === neighbor[1]))
             continue;
         path.push(neighbor);
-        markFrontier(neighbor);
-        let tmp = search({ map, g: g + map[neighbor[0]][neighbor[1]], threshold, path, node: neighbor, goal, markFrontier, markVisited });
+        let tmp = search({ map, g: g + map[neighbor[0]][neighbor[1]], threshold, path, node: neighbor, goal, markFrontier, markVisited, visited });
         if (tmp === true)
             return true;
         if (tmp < min)
             min = tmp;
         path.pop();
     }
-    markVisited(node);
     return min;
 }
 
 function ida_star({ map, start, goal, markFrontier, markVisited, startIteration }) {
+    let visited = Array.from(Array(map.length), () => new Array(map[0].length));
     let path = [start];
     let threshold = heuristic(start, goal);
     while (true) {
         startIteration();
-        let tmp = search({ map, g: 0, threshold, path, node: start, goal, markFrontier, markVisited });
+        let tmp = search({ map, g: 0, threshold, path, node: start, goal, markFrontier, markVisited, visited });
         if (tmp === true)
             return path;
         if (tmp === Infinity)
